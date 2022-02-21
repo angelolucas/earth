bd3b1dfbde28be51d47e2695dd291f6ed350f00d
Ângelo <angelo.lrc@gmail.com>
2022-02-21 20:26:24 -0300

    removes the blocks

--- scripts.js
+++ scripts.js
@@ -1,22 +1,33 @@
 let currentLength = 2;
 const meridians = 18; // vertical
 const parallels = 20; // horizontal
-const earth = document.querySelector('.earth');
+const earth = document.querySelector(".earth");
 
-const handleChange = (e) => {
-  const newLenght = e.target.value;
-  const allBlocks = earth.querySelectorAll('[data-index]');
+const handleAdd = (e) => {
   addBlock();
-}
+};
+
+const handleRemove = (e) => {
+  removeBlock();
+};
 
 const addBlock = () => {
-  const allBlocks = earth.querySelectorAll('[data-index]');
+  const allBlocks = earth.querySelectorAll("[data-index]");
   const numberOfBlocks = allBlocks.length;
   const lastBlock = allBlocks[numberOfBlocks - 1];
 
-  console.log(lastBlock);
-  const newBlock = document.createElement('div')
-  newBlock.className = "block"
-  newBlock.setAttribute('data-index', numberOfBlocks + 1)
-  lastBlock.appendChild(newBlock)
-}
+  const newBlock = document.createElement("div");
+  newBlock.className = "block";
+  newBlock.setAttribute("data-index", numberOfBlocks + 1);
+  lastBlock.appendChild(newBlock);
+};
+
+const removeBlock = () => {
+  const allBlocks = earth.querySelectorAll("[data-index]");
+  const numberOfBlocks = allBlocks.length;
+  const lastBlock = allBlocks[numberOfBlocks - 1];
+
+  if (numberOfBlocks > 1) {
+    lastBlock.remove();
+  }
+};
