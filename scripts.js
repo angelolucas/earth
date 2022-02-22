b68044916b26b9ff2dfcfacad57dfb77761ebb46
Ângelo <angelo.lrc@gmail.com>
2022-02-21 22:06:28 -0300

    renders x and y grid

--- scripts.js
+++ scripts.js
@@ -1,40 +1,50 @@
-let currentLength = 1;
-const meridians = 18; // vertical
-const parallels = 20; // horizontal
+let currentLength = 7;
 const earth = document.querySelector(".earth");
 
 const handleChange = (e) => {
-  const newValue = e.target.value;
+  const value = parseInt(e.target.value);
 
-  while (newValue > currentLength) {
-    addBlock();
-  }
-
-  while (newValue < currentLength) {
-    removeBlock();
-  }
+  renderSphere(value);
 };
 
-const addBlock = () => {
-  const allBlocks = earth.querySelectorAll(".block");
+const renderSphere = (value) => {
+  resetBlocks();
+
+  for (let step = 0; step < value; step++) {
+    addBlock(value);
+  }
+
+  currentLength = value;
+};
+
+window.onload = () => {
+  renderSphere(currentLength);
+};
+
+const resetBlocks = () => {
+  earth.querySelector(".earth .block")?.remove();
+};
+
+const addBlock = (value) => {
+  const allBlocks = earth.querySelectorAll(".block-x");
   const numberOfBlocks = allBlocks.length;
-  const lastBlock = allBlocks[numberOfBlocks - 1];
+  const lastBlock = allBlocks[numberOfBlocks - 1] || earth;
 
   const newBlock = document.createElement("div");
-  newBlock.className = "block";
-  newBlock.setAttribute("data-index", numberOfBlocks + 1);
+  newBlock.className = "block block-x";
   lastBlock.appendChild(newBlock);
 
-  currentLength++;
+  for (let step = 0; step < value - 1; step++) {
+    addYBlock(newBlock);
+  }
 };
 
-const removeBlock = () => {
-  const allBlocks = earth.querySelectorAll(".block");
+const addYBlock = (element) => {
+  const allBlocks = element.querySelectorAll(".block-y");
   const numberOfBlocks = allBlocks.length;
-  const lastBlock = allBlocks[numberOfBlocks - 1];
+  const lastBlock = allBlocks[numberOfBlocks - 1] || element;
 
-  if (numberOfBlocks > 1) {
-    lastBlock.remove();
-    currentLength--;
-  }
+  const newBlockY = document.createElement("div");
+  newBlockY.className = "block block-y";
+  lastBlock.appendChild(newBlockY);
 };
