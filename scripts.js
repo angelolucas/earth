c62301fb3d89a79647ef19c7a1ab21b3e4766d2f
Ângelo <angelo.lrc@gmail.com>
2022-02-28 13:35:04 -0300

    improve script readability

--- scripts.js
+++ scripts.js
@@ -1,73 +1,79 @@
 let currentLength = document.querySelector("input").value;
 const earth = document.querySelector(".earth");
+const SPHERE_WIDTH = 1439;
+const SPHERE_HEIGHT = 360;
+const blockWidth = SPHERE_WIDTH / currentLength;
+const blockHeight = SPHERE_HEIGHT / currentLength;
 
 const handleChange = (e) => {
-  const value = parseInt(e.target.value);
+  const totalSlices = parseInt(e.target.value);
 
-  renderSphere(value);
+  renderSphere(totalSlices);
 };
 
-const renderSphere = (value) => {
+const renderSphere = (totalSlices) => {
   resetBlocks();
 
-  for (let step = 0; step < value; step++) {
-    addSlice(value, step);
+  for (let currentSlice = 0; currentSlice < totalSlices; currentSlice++) {
+    addSlice({ totalSlices, currentSlice });
   }
 
-  updateBlockStyle(value);
+  updateBlockStyle(totalSlices);
 
-  currentLength = value;
-};
-
-window.onload = () => {
-  createStyle();
-  renderSphere(currentLength);
-};
-
-const createStyle = () => {
-  const style = document.createElement("style");
-  style.innerHTML = `.slice {} .block {}`;
-  document.head.appendChild(style);
+  currentLength = totalSlices;
 };
 
 const resetBlocks = () => {
   earth.querySelector(".earth .slice")?.remove();
 };
 
-const updateBlockStyle = (value) => {
+const updateBlockStyle = (totalSlices) => {
   const styleElement = document.querySelector("style");
   const slide = styleElement.sheet.cssRules[0].style;
   const block = styleElement.sheet.cssRules[1].style;
-  slide.width = `${2000 / value}px`;
-  block.height = `${520 / value}px`;
+  slide.width = `${SPHERE_WIDTH / totalSlices}px`;
+  block.height = `${SPHERE_HEIGHT / totalSlices}px`;
 
-  //slide.transform = `rotateY(${360 / value}deg)`;
-  //block.transform = `rotateX(-${90 / value}deg)`;
+  //slide.transform = `rotateY(${360 / totalSlices}deg)`;
+  //block.transform = `rotateX(-${90 / totalSlices}deg)`;
 };
 
-const addSlice = (value, columnIndex) => {
+const addSlice = ({ totalSlices, currentSlice }) => {
   const allBlocks = earth.querySelectorAll(".slice");
   const numberOfBlocks = allBlocks.length;
   const lastBlock = allBlocks[numberOfBlocks - 1] || earth;
 
-  const newSlide = document.createElement("div");
-  newSlide.className = "slice";
-  lastBlock.appendChild(newSlide);
+  const slice = document.createElement("div");
+  slice.className = "slice";
+  lastBlock.appendChild(slice);
 
-  for (let step = 0; step < value - 1; step++) {
-    addBlock(newSlide, columnIndex, step);
+  for (let currentBlock = 0; currentBlock < totalSlices - 1; currentBlock++) {
+    addBlock({ slice, currentSlice, currentBlock, totalSlices });
   }
 };
 
-const addBlock = (element, columnIndex, lineIndex) => {
-  const allBlocks = element.querySelectorAll(".block");
+const addBlock = ({ slice, currentSlice, currentBlock, totalSlices }) => {
+  const allBlocks = slice.querySelectorAll(".block");
   const numberOfBlocks = allBlocks.length;
-  const lastBlock = allBlocks[numberOfBlocks - 1] || element;
+  const lastBlock = allBlocks[numberOfBlocks - 1] || slice;
 
   const newBlock = document.createElement("div");
   newBlock.className = "block";
-  newBlock.style.backgroundPosition = `-${columnIndex * 66}px -${
-    lineIndex * 17
+  const bgXPosition = `-${(SPHERE_WIDTH / totalSlices) * currentSlice}px`;
+  const bgYPosition = `-${
+    (SPHERE_HEIGHT / totalSlices) * currentBlock + 360
   }px`;
+  newBlock.style.backgroundPosition = `${bgXPosition} ${bgYPosition}`;
   lastBlock.appendChild(newBlock);
+};
+
+const createStyle = () => {
+  const style = document.createElement("style");
+  style.innerHTML = `.slice{} .block{}`;
+  document.head.appendChild(style);
+};
+
+window.onload = () => {
+  createStyle();
+  renderSphere(currentLength);
 };
